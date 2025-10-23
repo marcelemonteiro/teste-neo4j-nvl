d606a905d8c290aa96235f727c683aeb02228e41
Marcele Monteiro <marcelepmoneiro@gmail.com>
2025-10-23 11:49:24 -0300

    Updates

---
+++ assets/index-faQnK6uG.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/index.umd-CeBYpJz-.js","assets/index-zkem9-yI.js"])))=>i.map(i=>d[i]);
+import{_ as n,a as o,s as a}from"./index-zkem9-yI.js";function _(i){return n(this,void 0,void 0,function(){var t;return o(this,function(e){switch(e.label){case 0:return[4,a(()=>import("./index.umd-CeBYpJz-.js").then(r=>r.i),__vite__mapDeps([0,1]))];case 1:return t=e.sent(),i._plugins=t,[2]}})})}export{_ as loadLegacyVideoPlugins};
