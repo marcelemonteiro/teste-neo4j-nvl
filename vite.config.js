60f1f3d662db360e5c251196707775638ca75f6b
Marcele Monteiro <marcelepmoneiro@gmail.com>
2025-10-23 11:47:32 -0300

    configure github pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,18 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 
 export default defineConfig({
-    optimizeDeps: {
-      exclude: ['@neo4j-nvl/layout-workers'],
-      include: [
-        '@neo4j-nvl/layout-workers > cytoscape',
-        '@neo4j-nvl/layout-workers > cytoscape-cose-bilkent',
-        '@neo4j-nvl/layout-workers > @neo4j-bloom/dagre',
-        '@neo4j-nvl/layout-workers > bin-pack',
-        '@neo4j-nvl/layout-workers > graphlib'
-      ],
-      esbuildOptions: {
-        target: 'es2020'
-      }
-    }
-  })
+  base: "/teste-neo4j-nvl/",
+  optimizeDeps: {
+    exclude: ["@neo4j-nvl/layout-workers"],
+    include: [
+      "@neo4j-nvl/layout-workers > cytoscape",
+      "@neo4j-nvl/layout-workers > cytoscape-cose-bilkent",
+      "@neo4j-nvl/layout-workers > @neo4j-bloom/dagre",
+      "@neo4j-nvl/layout-workers > bin-pack",
+      "@neo4j-nvl/layout-workers > graphlib",
+    ],
+    esbuildOptions: {
+      target: "es2020",
+    },
+  },
+});
